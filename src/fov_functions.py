ce202f753e0a0a3251af9cd5d685c63d3db0a531
shpaackle <shpaackle@gmail.com>
2019-03-09 17:03:27 -0500

    removed unnecessary old and commented code

--- src/fov_functions.py
+++ src/fov_functions.py
@@ -5,31 +5,7 @@ from map_objects import Point, Tile
 from map_objects.dungeon import Dungeon
 
 
-def initialize_fov(game_map: Dungeon, testing=False):
-
-    if testing:
-        m = tcod.map.Map(width=9, height=12, order="F")
-        print(f"walkable = {m.walkable}")
-
-        for y in range(12):
-            for x in range(9):
-                if (x == 3 and y == 5) or (x == 9 and y ==7):
-                    tile = Tile.wall(Point(x, y))
-                else:
-                    tile = Tile.floor(Point(x, y))
-
-                m.walkable[x, y] = tile.walkable
-                m.transparent[x, y] = tile.transparent
-        return m
-
-    # fov_map = tcod.map.Map(width=const.MAP_SETTINGS["map_width"], height=const.MAP_SETTINGS["map_height"], order="F")
-    #
-    # for point, tile in game_map:
-    #     fov_map.transparent[point.x, point.y] = tile.transparent
-    #     fov_map.walkable[point.x, point.y] = tile.walkable
-    #     # tcod.map_set_properties(fov_map, point.x, point.y, not tile.transparent, not tile.walkable)
-    #
-    # return fov_map
+def initialize_fov(game_map: Dungeon):
 
     for y in range(game_map.height):
         for x in range(game_map.width):
