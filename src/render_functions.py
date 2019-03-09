915707e0d55567b22e680ee57cbdfddf84c1bc40
shpaackle <shpaackle@gmail.com>
2019-03-09 15:22:28 -0500

    removed unnecessary print statements and old commented code

--- src/render_functions.py
+++ src/render_functions.py
@@ -9,42 +9,18 @@ from map_objects import Tile, Point
 def render_all(entities, game_map, fov_update,):
 
     if fov_update:
-        print(f"fov_update = {fov_update}")
-        print(f"game_map = {game_map}")
         terminal.clear()
         terminal.layer(Layers.MAP)
 
         for point, tile in game_map:
             visible = game_map.fov[point.x, point.y]
-            print(f"point {point} = {visible}")
             if visible:
-                print("visible")
                 color = terminal.color_from_name("white")
                 terminal.color(color)
             else:
                 color = terminal.color_from_name("grey")
                 terminal.color(color)
             terminal.put(point.x, point.y, tile.char)
-
-    # if not fov_update:
-    #     terminal.clear()
-    #     terminal.layer(Layers.MAP)
-    #     for y in range(12):
-    #         for x in range(9):
-    #             if (x == 3 and y == 5) or (x == 9 and y == 7):
-    #                 tile = Tile.wall(Point(x, y))
-    #             else:
-    #                 tile = Tile.floor(Point(x, y))
-    #
-    #             visible = fov_map.fov[x, y]
-    #             print(f"({x}, {y} is visible == {visible}")
-    #             if visible:
-    #                 color = terminal.color_from_name("white")
-    #                 terminal.color(color)
-    #             else:
-    #                 color = terminal.color_from_argb(100, 100, 100, 100)
-    #                 terminal.color(color)
-    #             terminal.put(x, y, tile.char)
 
     for entity in entities:
         draw_entity(entity)
